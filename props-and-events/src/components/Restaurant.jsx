e750e1683cf00bd85a9eec2b14e3537be287253e
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-04-08 12:42:16 -0700

    Completed the .map and the format for rendering the data.

--- props-and-events/src/components/Restaurant.jsx
+++ props-and-events/src/components/Restaurant.jsx
@@ -1,8 +1,15 @@
-function Restaurant() {
 
+function Restaurant(props) {
   return (
     <div className="restaurant">
-      
+      <img src={props.restaurant.image} alt={props.name} />
+      <h2>{props.restaurant.name}</h2>
+      <ul>
+        <li>{props.restaurant.address}</li>
+        <li>{props.restaurant.phone}</li>
+        <li>{props.restaurant.cuisine}</li>
+        <li>⭐ {props.restaurant.rating}</li>
+      </ul>
     </div>
   );
 }
--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,19 +1,13 @@
 // Importing Presentation Component
 import Restaurant from "./Restaurant.jsx";
 // Importing Data
-import restaurantData from "../data/data.js";
+import { restaurants } from "../data/data.js";
 
 function RestaurantsContainer() {
   return (
     <div className="restaurantContainer">
       {
-        restaurantData.map(restaurant => (
-          <Restaurant 
-            name={restaurant.name}
-            
-          
-          />
-        ))
+        restaurants.map(restaurant => (<Restaurant restaurant={restaurant} key={restaurant.id}/>))
       }
     </div>
   );
